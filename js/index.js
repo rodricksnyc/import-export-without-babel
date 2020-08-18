742b541a36b122845a465deb74d2ab1b0c7fb6c2
Elizabeth <elizabeth@Elizabeths-iMac.local>
2020-08-18 17:55:28 -0400

    open weather api

--- js/index.js
+++ js/index.js
@@ -102,41 +102,41 @@ $(".stickyBar .rel .corresponding").mouseleave(function(){
 
 
 
-	// var Ip = 'https://ipinfo.io/json';
-	//
-	// $.getJSON(Ip, function(data) {
-	// 	var city = data.city;
-	// 	var region = data.region;
-	// 	var country = data.country;
-	//
-	//
-	// 	var proxy = 'https://cors-anywhere.herokuapp.com/';
-	//
-	// 	var URL = 'http://api.openweathermap.org/data/2.5/weather?id=4367175&appid=b2f2b923be22181ef89baa544605b888'
-	//
-	//
-	// 	$.getJSON(proxy + URL, function(data) {
-	// 		var type = data.weather[0].main;  //array 0 index
-	// 		var id = data.weather[0].id; //array 0 index
-	// 		var city = data.name;
-	//
-	// 		var tempCel = Math.round(data.main.temp - 273.15);
-	// 		var tempC = tempCel + '°C';
-	// 		var weather = data.weather[0].description;
-	// 		// var tempF = Math.round(tempCel * (9 / 5) + 32) +  "<p>" +  "&deg; F" + "</p>" ;
-	// 		var icon = data.weather[0].icon;
-	// 		var tempBool = true;
-	//
-	// 		//Output data to display on the page
-	// 		$('#city').text(city);
-	// 		$('#state').text(region);
-	// 		$("#temp").html("<p>" + Math.round(tempCel * (9 / 5) + 32) +  "&deg; F" + "</p>");
-	// 		var weatherIcon = 'http://openweathermap.org/img/w/' + icon + '.png';
-	// 		$('#wIcon').html('<img src=' + weatherIcon + '>');
-	//
-	//
-	// 	});
-	// });
+	var Ip = 'https://ipinfo.io/json';
+
+	$.getJSON(Ip, function(data) {
+		var city = data.city;
+		var region = data.region;
+		var country = data.country;
+
+
+		var proxy = 'https://cors-anywhere.herokuapp.com/';
+
+		var URL = 'http://api.openweathermap.org/data/2.5/weather?id=4367175&appid=b2f2b923be22181ef89baa544605b888'
+
+
+		$.getJSON(proxy + URL, function(data) {
+			var type = data.weather[0].main;  //array 0 index
+			var id = data.weather[0].id; //array 0 index
+			var city = data.name;
+
+			var tempCel = Math.round(data.main.temp - 273.15);
+			var tempC = tempCel + '°C';
+			var weather = data.weather[0].description;
+			// var tempF = Math.round(tempCel * (9 / 5) + 32) +  "<p>" +  "&deg; F" + "</p>" ;
+			var icon = data.weather[0].icon;
+			var tempBool = true;
+
+			//Output data to display on the page
+			$('#city').text(city);
+			$('#state').text(region);
+			$("#temp").html("<p>" + Math.round(tempCel * (9 / 5) + 32) +  "&deg; F" + "</p>");
+			var weatherIcon = 'http://openweathermap.org/img/w/' + icon + '.png';
+			$('#wIcon').html('<img src=' + weatherIcon + '>');
+
+
+		});
+	});
 
 	//508 tabbing
 
